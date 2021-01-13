9296408b63b39e6c42eccfe8a58710b283ead786
Mykhaylo Lyubov <mykhaylo.lyubov@gmail.com>
2021-01-13 19:46:12 +0200

    made debounce for input with lodashDebounce

--- src/index.js
+++ src/index.js
@@ -3,25 +3,32 @@ import './styles.css';
 import listMarkup from './templates-handlebars/listMarkup.hbs';
 import refs from './js/refs';
 import getRequest from './js/apiService';
+import debounce from 'lodash.debounce';
 
-let pageNumber = 1;
+let pageNumber = null;
 let searchQuery = null;
 
-refs.form.addEventListener('input', event => {
+const debouncedInputCallback = debounce(event => {
   refs.gallery.innerHTML = '';
+  pageNumber = 1;
   searchQuery = event.target.value;
+  console.log(pageNumber);
 
   getRequest(searchQuery, pageNumber).then(({ data: { hits } }) =>
     refs.gallery.insertAdjacentHTML('beforeend', listMarkup(hits)),
   );
 
   return searchQuery;
-});
+}, 500);
 
-refs.loadMoreBtn.addEventListener('click', () => {
+function loadMoreButtonHandler() {
   pageNumber += 1;
-
+  console.log(pageNumber);
   getRequest(searchQuery, pageNumber).then(({ data: { hits } }) =>
     refs.gallery.insertAdjacentHTML('beforeend', listMarkup(hits)),
   );
-});
+}
+
+refs.form.addEventListener('input', debouncedInputCallback);
+
+refs.loadMoreBtn.addEventListener('click', loadMoreButtonHandler);
