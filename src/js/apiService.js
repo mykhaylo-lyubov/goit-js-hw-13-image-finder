f86b9a51201b463ca843cd17e96f431fcc50d894
Mykhaylo Lyubov <mykhaylo.lyubov@gmail.com>
2021-01-22 12:46:37 +0200

    remade with async function

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -7,12 +7,28 @@ const itemsPerPage = 12;
 axios.defaults.baseURL =
   'https://pixabay.com/api/?image_type=photo&orientation=horizontal';
 
-function getRequest(searchQuery, pageNumber) {
-  return axios
-    .get(
+// function getRequest(searchQuery, pageNumber) {
+//   if (searchQuery === '') {
+//     return;
+//   }
+//   return axios
+//     .get(
+//       `&key=${API_KEY}&q=${searchQuery}&page=${pageNumber}&per_page=${itemsPerPage}`,
+//     )
+//     .catch(error => console.log(error));
+// }
+
+const getRequest = async (searchQuery, pageNumber) => {
+  try {
+    if (searchQuery === '') {
+      return;
+    }
+    return await axios.get(
       `&key=${API_KEY}&q=${searchQuery}&page=${pageNumber}&per_page=${itemsPerPage}`,
-    )
-    .catch(error => console.log(error));
-}
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export default getRequest;
